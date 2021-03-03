bab7d8a801c8eed9657c6f83b42ed94ec64b373e
shuai hao <shuai.hao@shuaideMacBook-Pro.local>
2021-03-03 12:08:34 +0800

    [haoshuai] feat: method with ApplicationContext

--- src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingController.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingController.java
@@ -1,27 +1,37 @@
 package com.thoughtworks.capability.demospringioccontainer;
 
+import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Scope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import static org.springframework.beans.factory.config.ConfigurableBeanFactory.SCOPE_SINGLETON;
 
+/**
+ * 缺点1：耦合性高
+ * 缺点2；直接从容器中请求依赖项，所以它与控制反转的原理相矛盾
+ */
 @RestController
 @Scope(SCOPE_SINGLETON)
-public class GreetingController {
+public class GreetingController implements ApplicationContextAware {
 
-    private final GreetingService greetingService;
+    private ApplicationContext applicationContext;
 
-    @Autowired
-    public GreetingController(GreetingService greetingService) {
+    public GreetingController() {
         System.out.println("GreetingController init");
-        this.greetingService = greetingService;
     }
 
     @GetMapping("/greet")
     public String greet() {
-        return greetingService.sayHi();
+        return applicationContext.getBean(GreetingService.class).sayHi();
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
     }
 
 }
